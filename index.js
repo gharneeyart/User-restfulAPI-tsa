05f17a5a8fec046ea51a1796ad5638d7cd375473
Wealth <shuaibganiyat20@gmail.com>
2024-07-19 08:09:22 +0100

    cors

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(express.json());
 
 let corsOptions = { 
-    origin : ['http://localhost:5173', 'http://localhost:5174', 'https://fragrance-hub-eight.vercel.app'], 
+    origin : ['http://localhost:5173', 'http://localhost:5174'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true // Allow cookies and credentials
 } 
 app.use(cors(corsOptions));
 
