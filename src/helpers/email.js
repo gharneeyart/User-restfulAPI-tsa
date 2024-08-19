baba6b6acee4b01c27a4df86fd07c7905946ef8a
Wealth <shuaibganiyat20@gmail.com>
2024-08-19 14:30:52 +0100

    add email templates

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import { PASSWORD_RESET_REQUEST_TEMPLATE } from './emailTemplates.js';
 
 dotenv.config(); // 
 
@@ -12,12 +13,15 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendResetEmail = (email, resetLink) => {
+  const emailContent = PASSWORD_RESET_REQUEST_TEMPLATE.replace("{reserURL}", resetLink)
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: email,
     subject: 'Password Reset Request',
-    text: `You requested a password reset. Click the link below to reset your password:\n\n${resetLink}`,
-    html: `<p>You requested a password reset. Click the link below to reset your password:</p><p><a href="${resetLink}">${resetLink}</a></p>`,
+    // text: `You requested a password reset. Click the link below to reset your password:\n\n${resetLink}`,
+    // html: `<p>You requested a password reset. Click the link below to reset your password:</p><p><a href="${resetLink}">${resetLink}</a></p>`,
+    html: emailContent, // replace with your email template content.
+    category: "Reset Password",
   };
 
   return transporter.sendMail(mailOptions);
